cdcad5f82ea071241a7d4dafbc1de8773bbcb8bc
lialittis <yu-tianchi@outlook.com>
2024-05-01 21:24:32 +0800

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import typographyPlugin from '@tailwindcss/typography';
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--aw-color-primary)',
+        secondary: 'var(--aw-color-secondary)',
+        accent: 'var(--aw-color-accent)',
+        default: 'var(--aw-color-text-default)',
+        muted: 'var(--aw-color-text-muted)',
+      },
+      fontFamily: {
+        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
+        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [typographyPlugin,
+    require('daisyui')
+  ],
+  darkMode: 'class',
+  // daisyUI config (optional)
+	daisyui: {
+		styled: true,
+		themes: ["garden", "dark"],
+		base: true,
+		utils: true,
+		logs: false,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+	},
+};
