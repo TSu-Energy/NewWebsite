e1160477d7d90c1e9fc899acdd368c72fed0c298
lialittis <yu-tianchi@outlook.com>
2024-10-10 22:44:03 +0800

    update name

--- src/navigation.js
+++ src/navigation.js
@@ -225,6 +225,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/TS-energy"> TSuEnergy</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/TS-energy"> TSu-Energy</a> · All rights reserved.
   `,
 };
