853a569dd58ebc85e195904325bcc7c637a5c741
lialittis <yu-tianchi@outlook.com>
2024-11-30 16:05:40 +0100

    update footer

--- src/navigation.js
+++ src/navigation.js
@@ -86,39 +86,29 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: '解决方案和产品',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: '数字化绿色园区解决方案', href: '/solutions/industries' },
+        { text: '实验室与认证 AI 解决方案', href: '/solutions/labs' },
+        { text: '跨境贸易绿色解决方案', href: '/solutions/trade' },
+        { text: '绿境通 TSu-Pass™', href: '/products/ts-pass' },
+        { text: '天苏云 TSu-Cloud', href: '/products/ts-cloud' },
       ],
     },
     {
-      title: 'Platform',
+      title: '关于天苏能源',
       links: [
-        { text: 'Partners', href: '#' },
+        { text: '关于我们', href: '/about' },
+        { text: '天苏十条', href: '/category/tian1-su1-shi2-tiao2' },
+        { text: '天苏纵论', href: '/talk' },
+        { text: '天苏辑录', href: '/category/tian1-su1-ji2-lu4' },
+        { text: '天苏商城', href: '/sales' },
       ],
     },
     {
-      title: 'Support',
+      title: '立即咨询',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: '电子邮件： tsuenergy@outlook.com'},
       ],
     },
   ],
@@ -127,14 +117,13 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/TS-energy' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/TS-energy"> TSu-Energy</a> · All rights reserved.
   `,
 };
